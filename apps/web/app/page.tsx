87481825c78cf5f1b2585fe77b11964af974b26c
Debangi <debangiofficial@gmail.com>
2025-03-30 01:48:24 +0530

    updated frontend

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,8 @@ import { prismaClient } from "@repo/db/client";
 export default function Home() {
   return (
     <div>
-      <div>Hello World</div>;
+      <div>Hello World</div>; My name is Debangi. This is my test CI/CD
+      pipeline.
     </div>
   );
 }
