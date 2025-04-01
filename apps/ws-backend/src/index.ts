ea723cd9553d4560424e15e4c7698e9ff80ecd01
Debangi <debangiofficial@gmail.com>
2025-04-01 13:32:37 +0530

    Updated dummy ws logic

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -4,7 +4,14 @@ import { prismaClient } from "@repo/db/client";
 const server = new WebSocketServer({ port: 8080 });
 
 server.on("connection", async (socket) => {
-  socket.send("Connected to WebSocket server");
+  socket.send("Connected to WebSocket server at port: 8080");
+
+  socket.on("error", console.error);
+  socket.on("close", () => console.log("Connection closed"));
+  socket.on("message", async (data) => {
+    console.log(`Received message: ${data}`);
+    socket.send(`Received message: ${data}`);
+  });
 
   const res = await prismaClient.user.create({
     data: {
